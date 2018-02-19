09ceba26a4de6872c523514a02791c3eb591aa73
Catalin Atudori <catalin.atudori@ness.com>
2018-02-19 09:33:00 +0200

    Updated Assembly and File versions to 1.0.1.0.

--- Duplicati/Library/Backend/SharePoint/Properties/AssemblyInfo.cs
+++ Duplicati/Library/Backend/SharePoint/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ef2ffdff-aa60-477f-82a0-06d486c6dbbf")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
