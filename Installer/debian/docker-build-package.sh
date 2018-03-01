3155f7574fc59ebd8a3e7d85af6627d81ef2e68c
Nathan Johnson <njohnson@ena.com>
2018-03-01 08:41:45 -0600

    updating debian builder to move some files to enatrustbackup

--- Installer/debian/docker-build-package.sh
+++ Installer/debian/docker-build-package.sh
@@ -2,7 +2,7 @@
 
 set -e
 DATE=`date +%Y%m%d`
-VERSION=`git describe --tags | cut -d '-' -f 1 | cut -d 'v' -f 2`
+VERSION=$(git describe --tags | cut -d '-' -f 1 | cut -d 'v' -f 2 | sed -e 's/^release_\(.*\)_.*$/\1/')
 GITTAG=`git rev-parse --short HEAD`
 RELEASETYPE=`git describe --tags | cut -d '_' -f 2`
 BUILDTAG=`git describe --tags | cut -d '-' -f 2-4`
